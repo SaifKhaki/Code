1468cfd8c130beb370c8eeba0a81b9789f143c5d
saifbinkhaki <saifbinkhaki.official@gmail.com>
2021-05-30 13:32:23 +0500

    merged MLCollab (system info code) with the client code. Sent system info to the server right after connecting. Server storing all the system info of the clients in its list variables. added change tags.

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import psutil as p
 
 # Connecting to server's ip address
 ClientMultiSocket = socket.socket()
@@ -11,10 +12,44 @@ try:
 except socket.error as e:
     print(str(e))
 
-# first check response received 
+#:<change>
+def disk():
+    total_bytes = 0
+    free_bytes = 0
+    for x in p.disk_partitions():
+        dsk = p.disk_usage(x.mountpoint)
+        total_bytes += dsk.total
+        free_bytes += dsk.free
+    return (total_bytes, free_bytes)
+
+def memory():
+    mem = p.virtual_memory()
+    return (mem.total, mem.available)
+
+def cpu():
+    return (100-p.cpu_percent(), p.cpu_count(logical=True))
+
+# first check response received i.e "Server is working"
 res = ClientMultiSocket.recv(1024)
 print(res.decode('utf-8'))
 
+# sending system info
+messages_to_send = 6
+ClientMultiSocket.send(str.encode("rcv:"+str(messages_to_send)))
+ClientMultiSocket.send(str.encode(str(disk()[0])))
+ClientMultiSocket.send(str.encode(str(disk()[1])))
+ClientMultiSocket.send(str.encode(str(memory()[0])))
+ClientMultiSocket.send(str.encode(str(memory()[1])))
+ClientMultiSocket.send(str.encode(str(cpu()[0])))
+ClientMultiSocket.send(str.encode(str(cpu()[1])))
+#:</change>
+
+# receive acknowledge
+print("\n" + "/"*40 + " waiting for ack " + "/"*40)
+res = ClientMultiSocket.recv(1024)
+print(res.decode('utf-8'))
+print("/"*90 + "\n")
+    
 # series of many to one communication between server and client
 while True:
     messages_to_send = input('Write number of messages you want to send >>>> ')
--- server.py
+++ server.py
@@ -1,25 +1,60 @@
 import socket
 from _thread import *
 
-# giving a dynamic IP to the server
-ServerSideSocket = socket.socket()
+#:<change>
+# config
+max_clients = 10
+port = 2057
+
+# giving a dynamic IP and reuseable port to the server
+ServerSideSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ServerSideSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#:</change>
 host = socket.gethostname()
 print("The server's IP address is (add this to the client's input): ", socket.gethostbyname(host))
-port = 2057
-clientId = -1
-ThreadCount = 0
 try:
     ServerSideSocket.bind((host, port))
 except socket.error as e:
     print(str(e))
+    
+#:<change>
+# Global fields
+clientId = -1
+ThreadCount = 0
+disk_total = [0]*max_clients
+disk_free = [0]*max_clients
+ram_total = [0]*max_clients
+ram_free = [0]*max_clients
+cpu_free = [0]*max_clients
+cpu_cores = [0]*max_clients
+#:</change>
 
 # maximum number of clients to listen to
 print('Socket is listening..')
-ServerSideSocket.listen(10)
+ServerSideSocket.listen(max_clients)
 
 # function to be run in independent thread
 def multi_threaded_client(connection, id):
     connection.send(str.encode('Server is working:'))
+    
+    #:<change>
+    # receiving system info
+    data = connection.recv(2048)
+    if data.decode('utf-8').startswith("rcv:"):
+        count = int(data.decode('utf-8')[4:])
+        if(int(count) == 6):
+            disk_total[id] = float(connection.recv(2048).decode('utf-8'))
+            disk_free[id] = float(connection.recv(2048).decode('utf-8'))
+            ram_total[id] = float(connection.recv(2048).decode('utf-8'))
+            ram_free[id] = float(connection.recv(2048).decode('utf-8'))
+            cpu_free[id] = float(connection.recv(2048).decode('utf-8'))
+            cpu_cores[id] = int(connection.recv(2048).decode('utf-8'))
+
+            # sending ack
+            send_str = "ack: received following system info: Total_disk = " + str(disk_total[id]) + " Free_disk = " + str(disk_free[id]) + " Total_RAM = " + str(ram_total[id]) + " Free_RAM = " + str(ram_free[id]) + " Free_CPU = " + str(cpu_free[id]) + " CPU_Cores = " + str(cpu_cores[id])
+            connection.sendall(str.encode(send_str))
+    #:</change>
+
     while True:
         data = connection.recv(2048)
         response = 'Server message: ' + data.decode('utf-8')
