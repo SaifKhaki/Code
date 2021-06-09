a1a6755abd97690618a921b5f418f5382e18fa8d
saifbinkhaki <saifbinkhaki.official@gmail.com>
2021-06-09 17:29:00 +0500

    introduce yaml files of config and env, server is classifying all the connected nodes  to supernodes or ordinary nodes on the basis of download speed, gpu_ram, ram, cpu usage, and cpu cores

--- client.py
+++ client.py
@@ -1,26 +1,28 @@
 import socket
 import psutil as p
+import yaml
+import os
+import pynvml
+import speedtest
 
+# Read YAML file
+with open("config.yaml", 'r') as stream:
+    config_loaded = yaml.safe_load(stream)
+    
+# Global Fields
+status = ''
+    
 # Connecting to server's ip address
 ClientMultiSocket = socket.socket()
-host = input("Specify server's IP address: ")
-port = 2057
+host = config_loaded["server"]["ip"]
+port = config_loaded["server"]["port"]
+
 username = input("Input your username: ")
 print('Waiting for connection response')
 try:
     ClientMultiSocket.connect((host, port))
 except socket.error as e:
     print(str(e))
-
-#:<change>
-def disk():
-    total_bytes = 0
-    free_bytes = 0
-    for x in p.disk_partitions():
-        dsk = p.disk_usage(x.mountpoint)
-        total_bytes += dsk.total
-        free_bytes += dsk.free
-    return (total_bytes, free_bytes)
 
 def memory():
     mem = p.virtual_memory()
@@ -29,27 +31,52 @@ def memory():
 def cpu():
     return (100-p.cpu_percent(), p.cpu_count(logical=True))
 
+def gpu_memory():
+    try:
+        pynvml.nvmlInit()
+        # 1 here is the GPU id
+        handle = pynvml.nvmlDeviceGetHandleByIndex(1)
+        meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
+        return meminfo.free
+    except:
+        return 0
+
+def download_speed():
+    st = speedtest.Speedtest()
+    return st.download()/8
+    
 # first check response received i.e "Server is working"
 res = ClientMultiSocket.recv(1024)
 print(res.decode('utf-8'))
 
 # sending system info
-messages_to_send = 6
+messages_to_send = 5
 ClientMultiSocket.send(str.encode("rcv:"+str(messages_to_send)))
-ClientMultiSocket.send(str.encode(str(disk()[0])))
-ClientMultiSocket.send(str.encode(str(disk()[1])))
-ClientMultiSocket.send(str.encode(str(memory()[0])))
+
+# download speed in bytes
+ClientMultiSocket.send(str.encode(str(download_speed())))
+# available GPU ram in bytes
+ClientMultiSocket.send(str.encode(str(gpu_memory())))
+# available ram in bytes
 ClientMultiSocket.send(str.encode(str(memory()[1])))
+# cpu free in percentage
 ClientMultiSocket.send(str.encode(str(cpu()[0])))
+# cpu cores in count
 ClientMultiSocket.send(str.encode(str(cpu()[1])))
-#:</change>
 
 # receive acknowledge
 print("\n" + "/"*40 + " waiting for ack " + "/"*40)
-res = ClientMultiSocket.recv(1024)
-print(res.decode('utf-8'))
+res = ClientMultiSocket.recv(1024).decode('utf-8')
+print(res)
 print("/"*90 + "\n")
     
+# receive status in kazaa architecture
+status = ClientMultiSocket.recv(1024).decode('utf-8')[len("status:"):]
+print("\n" + "/"*40 + " waiting for ack " + "/"*40)
+print("You are appointed as " + ("Super node." if status=="s" else "Ordinary node."))
+print("/"*90 + "\n")
+
+
 # series of many to one communication between server and client
 while True:
     messages_to_send = input('Write number of messages you want to send >>>> ')
--- server.py
+++ server.py
@@ -1,59 +1,117 @@
 import socket
+import os
 from _thread import *
+import yaml
+import io
 
-#:<change>
-# config
-max_clients = 10
-port = 2057
+# Read YAML file
+with open("config.yaml", 'r') as stream:
+    config_loaded = yaml.safe_load(stream)
+
+# reading from config file
+port = config_loaded["server"]["port"]
+datasets = config_loaded["datasets"]
+s_nodes = config_loaded["kazaa"]["s_nodes"]
+o_nodes = config_loaded["kazaa"]["o_node_per"]
+all_nodes = s_nodes + (s_nodes*o_nodes)
+
+# Global fields
+clientId = -1
+ThreadCount = 0
+connections = []
+download_speed = [0]*all_nodes
+gpu_ram_free = [0]*all_nodes
+ram_free = [0]*all_nodes
+cpu_free = [0]*all_nodes
+cpu_cores = [0]*all_nodes
+status = ['']*all_nodes
+kazaa_constant = [0]*all_nodes
 
 # giving a dynamic IP and reuseable port to the server
 ServerSideSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ServerSideSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#:</change>
 host = socket.gethostname()
-print("The server's IP address is (add this to the client's input): ", socket.gethostbyname(host))
+print("The server's IP address: ", socket.gethostbyname(host))
 try:
     ServerSideSocket.bind((host, port))
 except socket.error as e:
     print(str(e))
-    
-#:<change>
-# Global fields
-clientId = -1
-ThreadCount = 0
-disk_total = [0]*max_clients
-disk_free = [0]*max_clients
-ram_total = [0]*max_clients
-ram_free = [0]*max_clients
-cpu_free = [0]*max_clients
-cpu_cores = [0]*max_clients
-#:</change>
+ServerSideSocket.listen(all_nodes)
 
-# maximum number of clients to listen to
-print('Socket is listening..')
-ServerSideSocket.listen(max_clients)
+# Write server host to config.yaml file
+config_loaded["server"]["ip"] = socket.gethostbyname(host)
+with io.open('config.yaml', 'w', encoding='utf8') as outfile:
+    yaml.dump(config_loaded, outfile, default_flow_style=False, allow_unicode=True)
+
+# giving minimum requirement of RAM for supernode
+def get_required_ram(fileSize, node="s"):
+    amount = fileSize/s_nodes
+    if(node == "o"):
+        amount /= o_nodes
+    return amount
+        
+def cal_kazaa_constant(id):
+    data_per_region = get_required_ram(size)
+    # /10 means to get data downloaded in max 10 secs
+    bytes_ps = data_per_region/10
+    kazaa_constant[id] = (((gpu_ram_free[id]+ram_free[id] >= data_per_region)*0.5)
+                          +(1 if (download_speed[id]/bytes_ps) >= 1 else (download_speed[id]/bytes_ps))*0.2
+                          +((gpu_ram_free[id]/(max(gpu_ram_free) if max(gpu_ram_free) != 0 else 1))*0.15)
+                          +((ram_free[id]/(max(ram_free) if max(ram_free) != 0 else 1))*0.075)
+                          +((cpu_free[id]/100)*0.05)
+                          +(1 if (cpu_cores[id]/8)>=1 else (cpu_cores[id]/8)*0.025)
+                          )
+
+# classifying nodes into supernode or ordinary node
+def get_node_status(id):
+    const = kazaa_constant[id]
+    a = kazaa_constant.copy()
+    a.sort()
+    if const in a[len(a)-s_nodes:]:
+        return "s"
+    else:
+        return "o"
+
+# initiating formation of kazaa architecture
+def initiate_kazaa():
+    for i in range(len(connections)):
+        cal_kazaa_constant(i)
+    
+    print("Constants calculated are: ", kazaa_constant)
+    
+    for i in range(len(connections)):
+        status[i] = get_node_status(i)
+        
+    print("Statuses calculated are: ", status)
+        
+    for connection in connections:
+        send_str = "status:" + status[connections.index(connection)]
+        connection.sendall(str.encode(send_str))
+        
+    
 
 # function to be run in independent thread
 def multi_threaded_client(connection, id):
-    connection.send(str.encode('Server is working:'))
+    connection.sendall(str.encode('Server is working:'))
     
-    #:<change>
     # receiving system info
     data = connection.recv(2048)
     if data.decode('utf-8').startswith("rcv:"):
         count = int(data.decode('utf-8')[4:])
-        if(int(count) == 6):
-            disk_total[id] = float(connection.recv(2048).decode('utf-8'))
-            disk_free[id] = float(connection.recv(2048).decode('utf-8'))
-            ram_total[id] = float(connection.recv(2048).decode('utf-8'))
+        if(count == 5):
+            download_speed[id] = float(connection.recv(2048).decode('utf-8'))
+            gpu_ram_free[id] = float(connection.recv(2048).decode('utf-8'))
             ram_free[id] = float(connection.recv(2048).decode('utf-8'))
             cpu_free[id] = float(connection.recv(2048).decode('utf-8'))
             cpu_cores[id] = int(connection.recv(2048).decode('utf-8'))
 
             # sending ack
-            send_str = "ack: received following system info: Total_disk = " + str(disk_total[id]) + " Free_disk = " + str(disk_free[id]) + " Total_RAM = " + str(ram_total[id]) + " Free_RAM = " + str(ram_free[id]) + " Free_CPU = " + str(cpu_free[id]) + " CPU_Cores = " + str(cpu_cores[id])
+            send_str = "ack: received following system info:  Download_speed = " + str(download_speed[id]) + "B Free_GPU_RAM = " + str(gpu_ram_free[id]) + "B Free_RAM = " + str(ram_free[id]) + "B Free_CPU = " + str(cpu_free[id]) + " CPU_Cores = " + str(cpu_cores[id])
             connection.sendall(str.encode(send_str))
-    #:</change>
+    
+    connections.append(connection)
+    if(len(connections) >= all_nodes):
+        initiate_kazaa()
 
     while True:
         data = connection.recv(2048)
@@ -61,7 +119,7 @@ def multi_threaded_client(connection, id):
         if not data:
             break
         elif data.decode('utf-8').startswith("rcv:"):
-            count = int(data.decode('utf-8')[4:])
+            count = int(data.decode('utf-8')[len("rcv:"):])
             for i in range(count):
                 data = connection.recv(2048).decode('utf-8')
                 response += " " + data
@@ -69,7 +127,12 @@ def multi_threaded_client(connection, id):
         connection.sendall(str.encode(response))
     connection.close()
 
+# assuming dataset uploaded by the user
+fileNumber = int(input("Which dataset you want to use from 0 - " + str(len(datasets)-1) + " : "))
+size = os.path.getsize(datasets[fileNumber]) 
+
 # always open for more clients to connect
+print('Socket is listening..')
 while True:
     Client, address = ServerSideSocket.accept()
     clientId += 1
