38bd19959a435f2ecbe35116eb9a137b5a99edcc
saifbinkhaki <saifbinkhaki.official@gmail.com>
2021-07-01 11:49:46 +0500

    Project Finished

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-a = []
-a[0] = 1
+def gpu_memory():
+    try:
+        pynvml.nvmlInit()
+        # 1 here is the GPU id
+        handle = pynvml.nvmlDeviceGetHandleByIndex(1)
+        meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
+        return meminfo.free
+    except:
+        return 0
+
+def download_speed():
+    try:
+        st = speedtest.Speedtest()
+        return st.download()/8
+    except:
+        return 0
